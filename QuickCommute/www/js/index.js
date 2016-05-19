e54ca10cfbbf0c2b03e281e273b6bc42bae29aa7
KavithaVishwanathan <kavivisu1612@gmail.com>
2016-05-19 02:57:04 -0400

    .

--- QuickCommute/www/js/index.js
+++ QuickCommute/www/js/index.js
@@ -1,4 +1,3 @@
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -48,5 +47,3 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-
-
